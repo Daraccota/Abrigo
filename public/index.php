108dba9019e5a8ab81cf63a51048bfaf7614d0df
Daraccota <dacidsouza@gmail.com>
2025-08-28 22:35:07 -0300

    Prototipo NAV
    Primeiro passo no tailwind

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="\Abrigo\assets\css\output.css" rel="stylesheet">
+  <title>document</title>
+</head>
+<body>
+  <nav class="h-20 bg-red-500 flex items-center justify-between ">
+
+    <div class="logo h-15 w-15 ml-1 bg-amber-300 flex-2">
+      <img class="h-15 w-15 justify-self-center" src="https://picsum.photos/200 " alt="" srcset="">
+    </div>
+
+    <div class="links justify-center align-items-center mx-20">
+      <ul class="flex gap-5 ">
+        <li>
+          <a class="" href="">Lorem Ipsium</a>
+        </li>
+        <li>
+          <a href="">Lorem Ipsium</a>
+        </li>
+        <li>
+          <a href="">Lorem Ipsium</a>
+        </li>
+        <li>
+          <a href="">Lorem Ipsium</a>
+        </li>      
+        <li>
+          <a href="">Lorem Ipsium</a>
+        </li>                        
+      </ul>
+    </div>
+    <button class="button h-10 w-20 bg-green-500 mr-30"></button>
+
+
+  </nav>
+</body>
+</html>
