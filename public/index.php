c4781309e6c1765349bf638e0155cb12eb9579e6
Daraccota <dacidsouza@gmail.com>
2025-08-31 02:34:59 -0300

    Fiz uma boa parte do header

--- public/index.php
+++ public/index.php
@@ -53,11 +53,46 @@
   <header class="bg-Creme">
     <img class="flex h-80 w-lvw justify-self-center color" 
     src="/Abrigo/assets/img/veio.png " alt="" srcset="">
-    <section class=" w-3/4 mx-7 mt-3 bg-pink-000"> 
-      <h1 class="tituloMobile text-CremeEscuro ">Há mais de 30 anos <br> 
-      <h2 class="textoMobile  text-CremeEscuro">acolhendo vidas com amor e dignidade</h2>
+
+    <!-- Seção com o texto inicial -->
+    <section class=" w-4/5 mx-7 mt-3 bg-pink-000"> 
+      <h1 class="tituloMobile text-CremeEscuro2">Há mais de 30 anos <br> 
+      <h2 class="subtituloMobile text-CremeEscuro2 mb-12">acolhendo vidas com amor e dignidade</h2>
       </h1>
+      <p class="textoMobile text-CremeEscuro2" >
+        O <span class="textoMobileSelecionado">Abrigo São Francisco de Assis</span> nasceu com a missão de oferecer cuidado,
+        proteção e qualidade de vida para pessoas idosas em situação de vulnerabilidade.
+        Nossa casa é mais do que um espaço físico: é um lar onde cada idoso encontra <span class="textoMobileSelecionado">carinho, segurança e respeito</span>.</p>
+        <p class="textoMobile text-CremeEscuro2">
+        Atuamos diariamente para garantir alimentação saudável, assistência à saúde, atividades sociais e muito afeto, 
+        sempre pautados pela transparência e pelo compromisso com a comunidade </p>
+      </p>
+
+      <!-- Botão "Fazer doação" -->
+      <div class="ml-4 botao inline-block ">
+        <div class="my-2 bg-CremeEscuro h-[1px] w-9/10 justify-self-center"></div>
+        <button class="textoBotao text-CremeEscuro2 bg-green-000">Fazer doação</button>
+        <div class="my-2 bg-CremeEscuro h-[1px] w-9/10 justify-self-center"></div>
+      </div>
     </section>
+
+    <div class="cards flex-1 justify-items-center">
+        <div class="card">
+          <div class="badge "></div>
+          <h1 class="">+30 anos de historia</h1>
+          <p class="">Experiencia e tradição no cuidado com pessoas idosas</p>
+        </div>
+        <div class="card">
+          <div class="badge "></div>
+          <h1 class="">+30 anos de historia</h1>
+          <p class="">Experiencia e tradição no cuidado com pessoas idosas</p>
+        </div>
+        <div class="card">
+          <div class="badge "></div>
+          <h1 class="">+30 anos de historia</h1>
+          <p class="">Experiencia e tradição no cuidado com pessoas idosas</p>
+        </div>
+    </div>
   </header>
 
 
