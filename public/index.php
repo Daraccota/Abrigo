ec52d93d1d91584092e563c588fcfeeaae76f928
Daraccota <dacidsouza@gmail.com>
2025-09-01 01:06:53 -0300

    Termieni o banner da pagina inicial

--- public/index.php
+++ public/index.php
@@ -78,17 +78,42 @@
 
     <div class="cards flex-1 justify-items-center">
         <div class="card">
-          <div class="badge "></div>
+          <div class="badge ">
+            <svg xmlns="http://www.w3.org/2000/svg" 
+              class="icons" 
+              fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <rect x="3" y="4" width="18" height="18" rx="2" ry="2"/>
+              <line x1="16" y1="2" x2="16" y2="6"/>
+              <line x1="8" y1="2" x2="8" y2="6"/>
+              <line x1="3" y1="10" x2="21" y2="10"/>
+            </svg>
+
+          </div>
           <h1 class="">+30 anos de historia</h1>
           <p class="">Experiencia e tradição no cuidado com pessoas idosas</p>
         </div>
         <div class="card">
-          <div class="badge "></div>
+          <div class="badge ">
+            <svg xmlns="http://www.w3.org/2000/svg" 
+              class="icons" 
+              fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+              <circle cx="9" cy="7" r="4"/>
+              <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
+              <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+            </svg>
+          </div>
           <h1 class="">+30 anos de historia</h1>
           <p class="">Experiencia e tradição no cuidado com pessoas idosas</p>
         </div>
         <div class="card">
-          <div class="badge "></div>
+          <div class="badge ">
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                class="icons" 
+                fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path d="M12 21c-4.418 0-8-3.582-8-8V7a2 2 0 0 1 2-2h1l1 5h2V5h2v5h2l1-5h1a2 2 0 0 1 2 2v6c0 4.418-3.582 8-8 8z"/>
+            </svg>
+          </div>
           <h1 class="">+30 anos de historia</h1>
           <p class="">Experiencia e tradição no cuidado com pessoas idosas</p>
         </div>
