ff9f39a88e51b8f6fa01a5e244bb0414edba555f
Daraccota <dacidsouza@gmail.com>
2025-09-05 01:24:34 -0300

    Carrossel estava escrito errado
    Mudança no nome da classe:
    carrosel -> carrossel

--- public/index.php
+++ public/index.php
@@ -170,10 +170,10 @@
         <!-- Carousel wrapper -->
         <div class="relative h-42 overflow-hidden rounded-lg md:h-96">
             <!-- Item 1 -->
-            <div class="itemcarrosel " data-carousel-item>
+            <div class="itemcarrossel " data-carousel-item>
                 <div class="bg-white w-6/10 h-8/10 rounded-xl absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
                     <div class="div"> 
-                      <p class="datacarrosel">12/08/2021</p>
+                      <p class="datacarrossel">12/08/2021</p>
                       <div class="flex"> <!-- Avaliação por estrelas --> <!-- TODO: Usar um loop no javascript pra isso, mas agora eu to muito cansado -->
                           <svg class="w-1/22 h-1/22 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
@@ -195,15 +195,15 @@
                         <img class=" rounded-full w-[14vw] h-[8vh] "src="https://picsum.photos/200" alt="">
                         <p class="text-[clamp(0.6rem,2vw,20px)] font-lufga font-medium ml-2">Todos os dias me sinto amada e lembrada</p>
                       </div>
-                      <p class="datacarrosel">Dona Helena, 72 anos</p>
+                      <p class="datacarrossel">Dona Helena, 72 anos</p>
                     </div>
                 </div>
             </div>
             <!-- Item 2 -->
-            <div class="itemcarrosel " data-carousel-item>
+            <div class="itemcarrossel " data-carousel-item>
                 <div class="bg-white w-6/10 h-8/10 rounded-xl absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
                     <div class="div"> 
-                      <p class="datacarrosel">12/08/2021</p>
+                      <p class="datacarrossel">12/08/2021</p>
                       <div class="flex"> <!-- Avaliação por estrelas --> <!-- TODO: Usar um loop no javascript pra isso, mas agora eu to muito cansado -->
                           <svg class="w-1/22 h-1/22 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
@@ -225,7 +225,7 @@
                         <img class=" rounded-full w-[14vw] h-[8vh] "src="https://picsum.photos/200" alt="">
                         <p class="text-[clamp(0.6rem,2vw,20px)] font-lufga font-medium ml-2">Todos os dias me sinto amada e lembrada</p>
                       </div>
-                      <p class="datacarrosel">Dona Helena, 72 anos</p>
+                      <p class="datacarrossel">Dona Helena, 72 anos</p>
                     </div>
                 </div>
             </div>
